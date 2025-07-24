a8d3ee00321df329f68dd8de23dad95ffb4af0d8
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-07-24 15:45:46 -0500

    Simplify assertions

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -6,13 +6,13 @@ import { confirmTransaction, RpcXyoConnection } from '@xyo-network/xl1-rpc'
 import { config } from 'dotenv'
 
 import { submitTransaction } from './submitTransaction.ts'
+
 // Load environment variables from .env file
 config({ quiet: true })
 
 export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost:8080/rpc'): Promise<void> {
   try {
     console.log('\n**** Starting XL1 Hello World NodeJs Sample ****\n')
-    // console.log(`\n**** With mnemonic:\n${mnemonic} ****\n`)
 
     // Create a HashPayload to send in the transaction
     const payload: HashPayload = {
@@ -23,12 +23,10 @@ export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost
     // Load the account to use for the transaction
     const walletMnemonic = assertEx(process.env.XYO_WALLET_MNEMONIC ?? mnemonic, () => 'Unable to resolve mnemonic from environment variable or argument')
     const account = await (await generateXyoBaseWalletFromPhrase(walletMnemonic)).derivePath(ADDRESS_INDEX.XYO)
-
     console.log('Using account:', account.address)
 
     // Load the RPC transport using the URL from the environment variable
-    const rpcEndpoint = assertEx(process.env.XYO_CHAIN_RPC_URL ?? endpoint, () => 'Unable to resolve RPC endpoint from environment variable or argument')
-
+    const rpcEndpoint = process.env.XYO_CHAIN_RPC_URL ?? endpoint
     console.log('Using endpoint:', rpcEndpoint)
 
     // Create a new RPC connection to the XL1 API Node
@@ -36,6 +34,8 @@ export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost
 
     // Send the transaction with the payload to the network via the Provider
     const txBW = await submitTransaction([payload], [], connection)
+
+    // Confirm the transaction is added to the chain
     confirmTransaction(connection, txBW, logSuccess)
   } catch (ex) {
     console.error('An error occurred:', isError(ex) ? ex.message : String(ex))
@@ -43,8 +43,7 @@ export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost
   }
 }
 
-// Confirm the transaction is added to the chain
-const logSuccess = () => {
+const logSuccess = (_hash?: string) => {
   console.log('To explore your local blockchain:\n')
   console.log('1. Install the XYO Layer One Wallet from https://chromewebstore.google.com/detail/xl1-wallet/fblbagcjeigmhakkfgjpdlcapcgmcfbm')
   console.log('2. In that same browser, go to: https://explore.xyo.network/xl1/local/')
