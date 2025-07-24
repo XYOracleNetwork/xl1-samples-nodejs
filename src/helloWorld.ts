a758755ed3fabf0807e106d8026cbf637fe52c6a
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-07-24 16:48:24 -0500

    Use async confirmation of submission

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,8 +1,8 @@
 import { assertEx } from '@xylabs/assert'
 import { isError } from '@xylabs/typeof'
-import type { HashPayload } from '@xyo-network/xl1-model'
+import type { HashPayload, HydratedTransaction } from '@xyo-network/xl1-model'
 import { ADDRESS_INDEX, generateXyoBaseWalletFromPhrase } from '@xyo-network/xl1-protocol-sdk'
-import { confirmTransaction, RpcXyoConnection } from '@xyo-network/xl1-rpc'
+import { confirmSubmittedTransaction, RpcXyoConnection } from '@xyo-network/xl1-rpc'
 import { config } from 'dotenv'
 
 import { submitTransaction } from './submitTransaction.js'
@@ -10,7 +10,7 @@ import { submitTransaction } from './submitTransaction.js'
 // Load environment variables from .env file
 config({ quiet: true })
 
-export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost:8080/rpc'): Promise<void> {
+export async function helloWorld(mnemonic?: string, rpcEndpoint = 'http://localhost:8080/rpc'): Promise<void> {
   try {
     console.log('\n**** Starting XL1 Hello World NodeJs Sample ****\n')
 
@@ -23,24 +23,25 @@ export async function helloWorld(mnemonic?: string, endpoint = 'http://localhost
     console.log('Using account:', account.address)
 
     // Load the RPC transport using the URL from the environment variable
-    const rpcEndpoint = process.env.XYO_CHAIN_RPC_URL ?? endpoint
-    console.log('Using endpoint:', rpcEndpoint)
+    const endpoint = process.env.XYO_CHAIN_RPC_URL ?? rpcEndpoint
+    console.log('Using endpoint:', endpoint)
 
     // Create a new RPC connection to the XL1 API Node
-    const connection = new RpcXyoConnection({ account, endpoint: rpcEndpoint })
+    const connection = new RpcXyoConnection({ account, endpoint })
 
     // Send the transaction with the Payload to the network via the Provider
     const txBW = await submitTransaction([payload], [], connection)
 
     // Confirm the transaction was added to the chain
-    await confirmTransaction(connection, txBW, logSuccess)
+    const confirmed = await confirmSubmittedTransaction(connection, txBW)
+    logSuccess(confirmed)
   } catch (ex) {
     console.error('An error occurred:', isError(ex) ? ex.message : String(ex))
     process.exitCode = 1
   }
 }
 
-const logSuccess = (_hash?: string) => {
+const logSuccess = (_tx: HydratedTransaction) => {
   console.log('To explore your local blockchain:\n')
   console.log('1. Install the XYO Layer One Wallet from https://chromewebstore.google.com/detail/xl1-wallet/fblbagcjeigmhakkfgjpdlcapcgmcfbm')
   console.log('2. In that same browser, go to: https://explore.xyo.network/xl1/local/')
