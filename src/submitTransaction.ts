822b126ef7966184b5f59c0821835c708686273a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2025-07-25 14:16:42 -0400

    remove deprecated deps

--- src/submitTransaction.ts
+++ src/submitTransaction.ts
@@ -1,14 +1,14 @@
 import { assertEx } from '@xylabs/assert'
-import type { Signed } from '@xyo-network/boundwitness-model'
 import type { Payload } from '@xyo-network/payload-model'
-import type { AllowedBlockPayload, TransactionBoundWitness } from '@xyo-network/xl1-model'
-import type { XyoConnectionProvider } from '@xyo-network/xl1-protocol'
+import type {
+  AllowedBlockPayload, TransactionBoundWitness, XyoConnectionProvider,
+} from '@xyo-network/xl1-protocol'
 
 export const submitTransaction = async (
   onChainPayloads: AllowedBlockPayload[],
   offChainPayloads: Payload[],
   provider: XyoConnectionProvider,
-): Promise<Signed<TransactionBoundWitness>> => {
+): Promise<TransactionBoundWitness> => {
   console.log('\n', '🚀 Sending transaction with payloads:', onChainPayloads, offChainPayloads, '\n')
 
   const [txBW] = await provider.submitTransaction?.(
