ea6bf14cc87d62bd0bf24579677a8a30458d4f9c
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-20 14:40:53 -0600

    Singleton gateway

--- src/getGateway.ts
+++ src/getGateway.ts
@@ -1,4 +1,5 @@
 import { assertEx } from '@xylabs/assert'
+import { isDefined } from '@xylabs/typeof'
 import type { XyoConnection } from '@xyo-network/xl1-sdk'
 import {
   ADDRESS_INDEX,
@@ -8,7 +9,11 @@ import {
 
 import { getLocator } from './getLocator.ts'
 
+let gateway: SimpleXyoGatewayRunner | undefined
+
 export const getGateway = async (mnemonic?: string, rpcEndpoint = 'http://localhost:8080/rpc') => {
+  if (isDefined(gateway)) return gateway
+
   // Load the account to use for the transaction
   const walletMnemonic = assertEx(process.env.XYO_WALLET_MNEMONIC ?? mnemonic, () => 'Unable to resolve mnemonic from environment variable or argument')
   const account = await (await generateXyoBaseWalletFromPhrase(walletMnemonic)).derivePath(ADDRESS_INDEX.XYO)
@@ -24,5 +29,6 @@ export const getGateway = async (mnemonic?: string, rpcEndpoint = 'http://localh
   const connection = await locator.getInstance<XyoConnection>(XyoConnectionMoniker)
 
   // Return a new SimpleXyoGatewayRunner instance
-  return new SimpleXyoGatewayRunner(connection, signer)
+  gateway = new SimpleXyoGatewayRunner(connection, signer)
+  return gateway
 }
