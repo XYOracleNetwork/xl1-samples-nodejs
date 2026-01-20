f9888e1d4b80943d0929a98245edc4a580996a36
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-20 15:01:05 -0600

    Use signer account helper

--- src/getGateway.ts
+++ src/getGateway.ts
@@ -1,13 +1,11 @@
-import { assertEx } from '@xylabs/assert'
 import { isDefined } from '@xylabs/typeof'
 import type { XyoConnection } from '@xyo-network/xl1-sdk'
 import {
-  ADDRESS_INDEX,
-  buildSimpleXyoSigner, generateXyoBaseWalletFromPhrase,
-  SimpleXyoGatewayRunner, XyoConnectionMoniker,
+  buildSimpleXyoSigner, SimpleXyoGatewayRunner, XyoConnectionMoniker,
 } from '@xyo-network/xl1-sdk'
 
 import { getLocator } from './getLocator.ts'
+import { getSignerAccount } from './getSignerAccount.ts'
 
 let gateway: SimpleXyoGatewayRunner | undefined
 
@@ -15,9 +13,7 @@ export const getGateway = async (mnemonic?: string, rpcEndpoint = 'http://localh
   // If existing gateway, return it
   if (isDefined(gateway)) return gateway
 
-  // Load the account to use for the transaction
-  const walletMnemonic = assertEx(process.env.XYO_WALLET_MNEMONIC ?? mnemonic, () => 'Unable to resolve mnemonic from environment variable or argument')
-  const account = await (await generateXyoBaseWalletFromPhrase(walletMnemonic)).derivePath(ADDRESS_INDEX.XYO)
+  const account = await getSignerAccount(mnemonic)
   console.log('Using account:', account.address)
 
   // Build the signer
