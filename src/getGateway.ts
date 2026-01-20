7204833c7390e32deae32bef6b2fe8792e5f0499
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-20 16:36:23 -0600

    Inline gateway creation

--- src/getGateway.ts
+++ src/getGateway.ts
@@ -1,25 +1,25 @@
-import { isDefined } from '@xylabs/typeof'
-import type { SimpleXyoSigner, XyoConnection } from '@xyo-network/xl1-sdk'
+import type { RpcSchemaMap, XyoConnection } from '@xyo-network/xl1-sdk'
 import {
-  SimpleXyoGatewayRunner, XyoConnectionMoniker, XyoSignerMoniker,
+  ADDRESS_INDEX, buildJsonRpcProviderLocator, generateXyoBaseWalletFromPhrase, HttpRpcTransport, SimpleXyoGatewayRunner, SimpleXyoSigner, XyoConnectionMoniker,
+  XyoSignerMoniker,
 } from '@xyo-network/xl1-sdk'
 
-import { getLocator } from './getLocator.ts'
-
-let gateway: SimpleXyoGatewayRunner | undefined
-
 export const getGateway = async (walletMnemonic: string, rpcUrl: string) => {
-  // If existing gateway, return it
-  if (isDefined(gateway)) return gateway
-
   // Get the provider locator
-  const locator = await getLocator(walletMnemonic, rpcUrl)
+  const transportFactory = (schemas: RpcSchemaMap) => new HttpRpcTransport(rpcUrl, schemas)
+  const locator = await buildJsonRpcProviderLocator({ transportFactory })
+
+  // Create new signer account
+  const wallet = await generateXyoBaseWalletFromPhrase(walletMnemonic)
+  const account = await wallet.derivePath(ADDRESS_INDEX.XYO)
+
+  // Register the signer with the locator
+  locator.register(SimpleXyoSigner.factory<SimpleXyoSigner>(SimpleXyoSigner.dependencies, { account }))
 
   // Use locator to get connection and signer
   const connection = await locator.getInstance<XyoConnection>(XyoConnectionMoniker)
   const signer = await locator.getInstance<SimpleXyoSigner>(XyoSignerMoniker)
 
   // Create gateway from connection and signer
-  gateway = new SimpleXyoGatewayRunner(connection, signer)
-  return gateway
+  return new SimpleXyoGatewayRunner(connection, signer)
 }
