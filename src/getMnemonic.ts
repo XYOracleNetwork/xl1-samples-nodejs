2d9bdad8d2ad1c40e8ee41229adbdf3dda186629
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-01-20 17:14:19 -0600

    ENV Helpers

---
+++ src/getMnemonic.ts
@@ -0,0 +1,14 @@
+import { HDWallet } from '@xyo-network/wallet'
+import { config } from 'dotenv'
+
+// Load environment variables from .env file
+config({ quiet: true })
+
+// Parse the relevant ENV VARs or use defaults
+const mnemonic = process.env.XYO_WALLET_MNEMONIC ?? HDWallet.generateMnemonic()
+
+/**
+ * Gets the mnemonic to use for the signer
+ * @returns The mnemonic to use for the signer
+ */
+export const getMnemonic = (): string => mnemonic
