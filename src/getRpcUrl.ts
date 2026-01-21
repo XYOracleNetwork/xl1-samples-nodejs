d26ef8d42bba862a3177aa812174acc548b8ff55
Matt Jones <885417+jonesmac@users.noreply.github.com>
2026-01-21 09:17:59 -0500

    match getter file format

--- src/getRpcUrl.ts
+++ src/getRpcUrl.ts
@@ -3,8 +3,11 @@ import { config } from 'dotenv'
 // Load environment variables from .env file
 config({ quiet: true })
 
+// Parse the relevant ENV VARs or use defaults
+const rpcUrl = process.env.XYO_CHAIN_RPC_URL ?? 'http://localhost:8080/rpc'
+
 /**
  * Gets the rpcUrl to use for interacting with the chain
  * @returns The rpcUrl to use for interacting with the chain
  */
-export const getRpcUrl = (): string => process.env.XYO_CHAIN_RPC_URL ?? 'http://localhost:8080/rpc'
+export const getRpcUrl = (): string => rpcUrl
